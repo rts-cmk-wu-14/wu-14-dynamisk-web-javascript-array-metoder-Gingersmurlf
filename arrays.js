e55ae7628e557ef49a1d309cd3b0bdfee9e0637a
Gingersmurlf <theodorenghoff@gmail.com>
2025-08-25 09:30:41 +0200

    opgave 4, 5, 6 og 7 løst

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
+    let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny",];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
@@ -22,30 +22,35 @@ document.addEventListener("DOMContentLoaded", function () {
     // Check om der i dogsArray er en hund som hedder "Trixie".
     // Udskriv "true" eller "false" i konsollen afhængig af svaret.
     // console.log("Opgave 3a", ...)
-    console.log("Opgave 3", dogsArray.includes("Trixie"));
+    console.log("Opgave 3a", dogsArray.includes("Trixie"));
     
     // Opgave 3b
     // Check om der i dogsArray er en hund som hedder "Baxter".
     // Udskriv "true" eller "false" i konsollen afhængig af svaret.
     // console.log("Opgave 3b", ...)
-    
+    console.log("Opgave 3b", dogsArray.includes("Baxter"))
 
     //     Opgave 4a
     // Check om der i catsArray er en kat som hedder "Bagheera".
     // Udskriv katten i konsollen hvis den findes og ellers "undefined".
     // console.log("Opgave 4a", ...)
+    const found = catsArray.find((element) => element === "Bagheera");
+    console.log("Opgave 4a", found);
     
 
     // Opgave 4b
     // Check om der i catsArray er en kat som hedder "Salem".
     // Udskriv katten i konsollen hvis den findes og ellers "undefined".
     // console.log("Opgave 4b", ...)
-    
+    const found2 = catsArray.find((element) => element === "Salem");
+    console.log("Opgave 4b", found2);
 
     //     Opgave 5
     // Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle tallene i arrayet ganget med 3.
     // Udskriv det nye array i konsollen:
     // console.log("Opgave 5", ...)
+    const map = numbersArray.map((x) => x *3)
+    console.log("opgave 5", map);
     
 
     //     Opgave 6
@@ -53,16 +58,18 @@ document.addEventListener("DOMContentLoaded", function () {
     // (Sådan her: BarneyMollyBaxter...)
     // Udskriv strengen konsollen:
     // console.log("Opgave 6", ...)
+    console.log("Opgave 6", dogsArray.join(""));
     
 
     //     Opgave 7a
     // Find ud af hvilket index "Mango" har i fruitsArray.
     // Udskriv indexet i konsollen.
-    
+    console.log("Opgave 7a", fruitsArray.indexOf("Mango"))
 
     // Opgave 7b
     // Find ud af hvilket index "Blåbær" har i fruitsArray.
     // Udskriv indexet i konsollen.
+    console.log("Opgave 7b", fruitsArray.indexOf("Blåbær"));
     
 
     // Opgave 8
